afe8bc50d77ac72e275bf67f61b5b8719b5077ea
Lucas Dachman <lucas.dachman@gmail.com>
2019-02-01 18:51:07 -0700

    landing page layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,18 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <main>
+    <main className='landing-main'>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <header className='landing-header'>
         <h1>Lucas</h1>
         <h1>Dachman</h1>
+        <p className='landing-blurb'>Software Developer, Front-End, Mobile Applications, Music Technology</p>
       </header>
-      <p className='landing-blurb'>Software Developer, Front-End, Mobile Applications, Music Technology</p>
-      <Link to="/page-2/">Go to page 2</Link>
+      <nav>
+        <Link to="/page-2/" className='button'>Works</Link>
+        <Link to="/page-2/" className='button'>Resume</Link>
+        <Link to="/page-2/" className='button'>Github</Link>
+      </nav>
     </main>
   </>
 )
