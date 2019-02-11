3c0a85fcba584d257603ff7b9da4eda30b8f8061
Lucas Dachman <lucas.dachman@gmail.com>
2019-02-10 17:55:01 -0700

    basic detail view layout

--- src/components/nav-header.js
+++ src/components/nav-header.js
@@ -5,7 +5,7 @@ export default (props) => (
   <nav className="nav-header">
       <Link to='/'>Lucas Dachman</Link>
       <Link to='/works/' className='current'>Works</Link>
-      <Link>Resume</Link>
+      <a href="https://github.com/lucasdachman">Resume</a>
       <a href="https://github.com/lucasdachman">Github</a>
   </nav>
 )
