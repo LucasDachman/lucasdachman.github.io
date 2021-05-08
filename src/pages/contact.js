c9981dd1ecfb281b03cebbb1cda819c98745c986
Lucas Dachman <lucas.dachman@gmail.com>
2021-05-08 16:29:32 -0600

    formatting

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,20 +1,31 @@
-import React from "react"
-import NavHeader from '../components/nav-header'
-import SEO from "../components/seo"
+import React from "react";
+import NavHeader from "../components/nav-header";
+import SEO from "../components/seo";
 
 const SecondPage = () => (
   <>
     <SEO title="Contact" />
-    <NavHeader current='contact'/> 
-    <main className='contact-main'>
+    <NavHeader current="contact" />
+    <main className="contact-main">
       <h1>Contact Me</h1>
       <form method="POST" action="https://formspree.io/lucas.dachman@gmail.com">
-        <input className='email-input' type="email" name="email" placeholder="Your email" />
-        <textarea className='email-msg-input' name="message" placeholder="Your Message"></textarea>
-        <button className='button' type="submit">Send</button>
+        <input
+          className="email-input"
+          type="email"
+          name="email"
+          placeholder="Your email"
+        />
+        <textarea
+          className="email-msg-input"
+          name="message"
+          placeholder="Your Message"
+        ></textarea>
+        <button className="button" type="submit">
+          Send
+        </button>
       </form>
     </main>
   </>
-)
+);
 
-export default SecondPage
+export default SecondPage;
